9f836224a7f96db4f9e9492d39967449c0573a4e
simosuomala-lab <simo.suomala@gmail.com>
2026-08-28 21:15:58 +0300

    Delete sw.js

--- sw.js
+++
@@ -1,1 +0,0 @@
-const C="tunnetyokortit-v1",A=["./","./index.html","./manifest.json"];self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
